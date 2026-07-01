a5da7b30e140f9a7a0678a720d76f9d6d0464b85
Noor Shurrab <fdlnwr88@gmail.com>
2026-07-01 14:39:37 +0300

    Complete models.py

--- core_app/models.py
+++ core_app/models.py
@@ -1,1 +1,15 @@
 from accounts_app.models import Testimonial
+from django.db import models
+
+class ContactMessage(models.Model):
+    name       = models.CharField(max_length=100)
+    email      = models.EmailField()
+    message    = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_read    = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.name} — {self.email}"
+    
+    class Meta:
+        ordering = ['-created_at']
