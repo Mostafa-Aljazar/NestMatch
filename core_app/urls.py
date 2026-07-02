865fea09452ced192b0d3e7f3df0a336310c954d
Noor Shurrab <fdlnwr88@gmail.com>
2026-07-02 16:34:04 +0300

    add privacy_policy path in urls.py file

--- core_app/urls.py
+++ core_app/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('faq/', views.faq, name='faq'),
     path('contact/', views.contact, name='contact'),
+    path('privacy-policy/', views.privacy_policy, name='privacy_policy'),
 ]
