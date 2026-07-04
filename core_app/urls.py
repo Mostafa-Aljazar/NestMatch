891d6d76d548933afa193038757054acefe6a661
Noor Shurrab <fdlnwr88@gmail.com>
2026-07-04 20:50:41 +0300

    add terms of use template

--- core_app/urls.py
+++ core_app/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('faq/', views.faq, name='faq'),
     path('contact/', views.contact, name='contact'),
     path('privacy-policy/', views.privacy_policy, name='privacy_policy'),
+    path('terms-of-use/', views.terms_of_use, name='terms_of_use'),
 ]
